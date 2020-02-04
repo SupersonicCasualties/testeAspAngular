f49c020a219dd01007cf06f8fc070aba4daf848a
Lucas S. Veque <lucasveque@hotmail.com>
2020-02-04 13:55:06 -0300

    Criado model de Pedido e Pedido Item

---
+++ BackEnd/AspNetWebApi/AspNetWebApi/Models/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetWebApi.Models
+{
+    public class Pedido : BaseModelo
+    {
+        [Required]
+        public Cliente Cliente { get; set; }
+
+        public decimal ValorTotal { get; set; }
+
+        public decimal Desconto { get; set; }
+
+        //[Required]
+        //public CondicaoPagamento condicaoPagamento { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ BackEnd/AspNetWebApi/AspNetWebApi/Models/PedidoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApi.Models
+{
+    public class PedidoItem : BaseModelo
+    {
+        [Required]
+        public Pedido Pedido { get; set; }
+
+        [Required]
+        public Produto Produto { get; set; }
+
+        public decimal ValorUnitario { get; set; }
+
+        public decimal ValorBruto { get; set; }
+
+        public decimal ValorLiquido { get; set; }
+
+        public int Quantidade { get; set; }
+
+    }
+}
